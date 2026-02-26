614788a9c82907147d18c3941c1aed39997b0356
YassineMH10 <mahamideyassine@gmail.com>
2026-02-26 21:27:15 +0000

    exports.py

--- modules/exports.py
+++ modules/exports.py
@@ -1,11 +1,17 @@
 # modules/exports.py
-
 import pandas as pd
 import io
+from modules.pda_store import load_actions
+
 
 def export_excel(df):
     buffer = io.BytesIO()
-    with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
+    actions = load_actions()
+    df_actions = pd.DataFrame(actions) if actions else pd.DataFrame()
+
+    with pd.ExcelWriter(buffer, engine="xlsxwriter") as writer:
         df.to_excel(writer, sheet_name="Données KPI", index=False)
+        df_actions.to_excel(writer, sheet_name="PDA", index=False)
+
     buffer.seek(0)
     return buffer
